ce6d37c96c8a7494da0b1c2137dd38a38e26ac3f
nikjke <nikjke@mail.ru>
2019-06-20 19:25:48 +0400

    Сделал рефакторинг кода

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // charCodeAt - возвращает unicode    A (sym)       =  65 (unicode)
 // fromCharCode - наоборот            65 (unicode)  =  A (sym)
 
-document.getElementById('input-sentence').oninput = function() {
+document.getElementById('input-sentence').oninput = function () {
     const offset = 3;
     let str = this.value;
     let out = '';
-    
+
     for (let i = 0; i < str.length; i++) {
         let code = str.charCodeAt(i);
         code = code + offset;
@@ -15,11 +15,11 @@ document.getElementById('input-sentence').oninput = function() {
     document.getElementById('output-unicode').innerHTML = out.split('#').join('a');
 }
 
-document.getElementById('input-unicode').oninput = function() {
+document.getElementById('input-unicode').oninput = function () {
     const offset = 3;
     let str = this.value;
     let out = '';
-    
+
     for (let i = 0; i < str.length; i++) {
         let code = str.charCodeAt(i);
         code = code - offset;
